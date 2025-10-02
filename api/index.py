782486623ff26ecbe7a05ff4b056f2a98c5fcc9d
phattharawadee24 <phattharawadeess283@gmail.com>
2025-10-02 21:36:54 +0700

    pp

--- api/index.py
+++ api/index.py
@@ -2,5 +2,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cherryon_top.settings")
 app = get_wsgi_application()
