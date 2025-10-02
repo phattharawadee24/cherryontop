ba8618bd080c6d377da80007a3f502de72072dcc
phattharawadee24 <phattharawadeess283@gmail.com>
2025-10-02 22:00:11 +0700

    Simplify Vercel configuration and fix build issues

--- api/index.py
+++ api/index.py
@@ -1,6 +1,2 @@
-# api/index.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cherryon_top.settings")
-app = get_wsgi_application()
+from myapp.wsgi import application
+app = application
