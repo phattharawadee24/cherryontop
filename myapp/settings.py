99f8fe8735ac24e94f9eb94fbee14c342bfe5dea
phattharawadee24 <phattharawadeess283@gmail.com>
2025-10-02 21:44:21 +0700

    pp

--- myapp/settings.py
+++ myapp/settings.py
@@ -17,6 +17,9 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# For Vercel deployment
+IS_VERCEL = os.environ.get('VERCEL', False)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -28,7 +31,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', '*']
 
 # Application definition
 INSTALLED_APPS = [
@@ -76,12 +79,24 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if IS_VERCEL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres.ahipddixeoyqxrlugbgb',
+            'PASSWORD': os.environ.get('SUPABASE_DB_PASSWORD', '0625170235Mm#'),
+            'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -117,6 +132,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/statics'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Vercel static files config
+if IS_VERCEL:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
